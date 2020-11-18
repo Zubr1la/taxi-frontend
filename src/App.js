457ec310da69448827655b725383f8175a0369d4
Andris Kudurs <kudurs3@Inbox.lv>
2020-11-18 19:41:27 +0200

    IDK WHERE THIS GOES

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Home from './Routes/Home';
+import NotFound from "./Routes/NotFound";
+import NavBar from "./Components/NavBar";
+import Panel from "./Routes/Panel";
+import Login from "./Routes/Login";
+import Register from "./Routes/Register";
+import UserSettings from "./Routes/UserSettings";
+import Messages from "./Routes/Messages";
+import MainPage from "./Routes/MainPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div>
+              <NavBar/>
+              <Switch>
+                  <Route exact path="/">
+                      <Home />
+                  </Route>
+                  <Route exact path="/orderdrive">
+                      <MainPage />
+                  </Route>
+                  <Route exact path="/login">
+                      <Login />
+                  </Route>
+                  <Route exact path="/register">
+                      <Register />
+                  </Route>
+                  <Route exact path="/panel">
+                      <Panel />
+                  </Route>
+                  <Route exact path="/usersettings">
+                      <UserSettings />
+                  </Route>
+                  <Route exact path="/messages">
+                      <Messages />
+                  </Route>
+                  <Route component={NotFound}/>
+              </Switch>
+
+          </div>
+      </Router>
   );
 }
 
---
+++ src/Routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Default from "../Components/Default";
+
+class Home extends React.Component {
+    render() {
+        return <div className='container'>
+            <Default/>
+        </div>
+    }
+}
+
+export default Home;
